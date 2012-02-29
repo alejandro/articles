c8ec49498c34049b5d97fb37ad649124d3df34f4
Alejandro Morales <vamg008@gmail.com>
2012-02-28 22:42:09 -0600

    [master] Function server 0.0.1

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var express = require('express'),
     app = module.exports =  express.createServer( express.cookieParser(),
   express.session({ secret: 'keyboard cat' })),
     groups =[];
+    var fs = require('fs');
+    var path = require('path');
+    var ghm = require('github-flavored-markdown');
 /* A little hack for correct handle of session */
   
 app.configure(function(){
@@ -34,21 +37,84 @@ app.dynamicHelpers({
 });
 
 app.get('/', function(req, res){
-  res.send('ok')
+  res.writeHeader(200,{'Content-type':'text/html'});
+  res.write('<!doctype html>')
+  res.write('<html>')
+  res.write('<head><meta charset="utf8" /><title>Articulos</title>')
+  res.write('<link href="/stylesheets/markdown.css" rel="stylesheet" /></head>')
+  res.write('<body>')
+  res.write('<article class="markdown-body">')
+  res.write('<h1>Hola los articulos disponibles son:</h1><ul><li>');
+  res.write(months().map(function(month){
+            var _data = '';
+            var posts =fs.readdirSync(path.join(__dirname,'posts',month));
+            _data +='<ul> <h3> '+month+':</h3><li>'
+            var s = posts.map(function(v){
+              return '<a href="/posts/'+parseInt(month.substr(-4))+'/'+month.substr(0,3)+'/'+v+ '">'+ v+'</a>';
+            }).join('</li><li>')
+            _data += s;
+            _data +='</li> </ul>'
+            return _data;
+          }).join('</li><li>'));
+  res.write('</article>')
+  res.write('<footer>Archive made by <a href="//alejandromorales.co.cc">Alejandro Morales</a></footer>')
+  res.end('</body></html>');
 });
-
-app.get('/a/:art',function(req,res){
-  var fs =require('fs');
-  var md = require('marked'); 
-  var art = req.params.art;
-  fs.readFile(__dirname + '/' + art, function(e,d){
-    res.end(md(d));
-  });
+var months = function(){
+  return fs.readdirSync(path.join(__dirname,'/posts'));
+};
+var shortMonths = {
+  'ene':'enero',
+  'feb':'febrero',
+  'mar':'marzo',
+  'abr':'abril',
+  'may':'mayo',
+  'jun':'junio',
+  'jul':'julio',
+  'ago':'agosto',
+  'sep':'septiembre',
+  'oct':'octubre',
+  'nov':'noviembre',
+  'dic':'diciembre'
+}
+app.get('/posts/:year/:month/:id',function(req,res){
+  var year = req.params.year;
+  var m = req.params.month;
+  var id = req.params.id;
+  var re = m + year;
+  if (shortMonths[m] !== undefined && months().indexOf(re) !==-1  ){ 
+    // Read the post?
+    fs.readFile(path.join(__dirname,'posts',re, id,'README.md'), 'utf8', function(e,d){
+      if (e && e.code == 'ENOENT'){
+        res.end('404 NOT_FOUND');
+      } else if (e){
+        res.end('500, INTERNAL SERVER ERROR');
+      } else {
+        var post ='<h1> Archivo mal formado</h1>';
+        try {
+          post = ghm.parse(d || '# POST NOT FOUND');
+        } catch(e){}
+        res.writeHeader(200,{'Content-type':'text/html'});
+        res.write('<!doctype html>')
+        res.write('<html>')
+        res.write('<head><meta charset="utf8" /><title>' + re +id +  '</title>')
+        res.write('<link href="/stylesheets/markdown.css" rel="stylesheet" /></head>')
+        res.write('<body>')
+        res.write('<header><a href="/"> Regresar</a></header>')
+        res.write('<article class="markdown-body">')
+        res.write(post)
+        res.write('</article>')
+        res.end('</body></html>');
+      }
+    });
+  } else {
+    res.json({
+      month:m,
+      year:year,
+      status:'not_found'
+    });
+  }
 });
-app.get('/login', function(req, res){
-  res.render('login',{title:'Hol'});
-});
-
 app.post('/login', function(req, res){
 });
 // Regresa el index num de un valor en una array como el de un 
